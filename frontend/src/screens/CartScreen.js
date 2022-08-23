cdec9020d46fc41aa000e4b9ec49b913709e3da8
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-08-23 15:16:08 -0400

    started remove from cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -31,6 +31,9 @@ function CartScreen() {
     }
   }, [dispatch, productId, qty]);
 
+  const removeFromCartHandler = (id) => {
+    console.log("remove: ", id);
+  };
   return (
     <Row>
       <Col md={8}>
@@ -53,6 +56,31 @@ function CartScreen() {
                     </Link>
                   </Col>
                   <Col md={2}>${item.price}</Col>
+                  <Col md={3}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(addToCart(item.product, e.target.value))
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+
+                    <Col md={2}>
+                      <Button
+                        type="button"
+                        variant="light"
+                        onClick={() => removeFromCartHandler(item.product)}
+                      >
+                        <i className="fa fa-trash"></i>
+                      </Button>
+                    </Col>
+                  </Col>
                 </Row>
               </ListGroup.Item>
             ))}
