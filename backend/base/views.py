25bdaec1ab523e183ed69104373df0c815322ea6
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-08-25 15:06:55 -0400

    create user

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,30 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password
 
 
-from .models import Product
+from .models import Product, User
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 # Create your views here.
 
 
-
-@api_view(['GET'])
-def getRoutes(request):
-    routes=['/api/products',
-            'api/products/create',
-            'api/products/upload',
-            'api/products/<id>/reviews',
-            'api/products/top', 
-            'api/products/<id>',
-            'api/products/delete/<id>',
-              'api/products/<update>/<id>',
-            ]
-    return Response(routes)
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -46,7 +35,29 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
     
+    
+@api_view(['POST'])    
+def registerUser(request):
+    data=request.data
+    user=User.objects.create(
+       #first_name = data['name']
+       username = data['email'], 
+       email = data['email'],
+       password = make_password(data['password'])
+    )
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+    
+ 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all() 
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user #pulling data from token b/c of decorator (not same user as admin panel)
     serializer = UserSerializer(user, many=False)
