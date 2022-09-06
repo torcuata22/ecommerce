3a42cc095c790dad744259da853f96647082692f
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-09-06 17:10:05 -0400

    create order, beginning

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -9,6 +9,53 @@ from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
 
-from base.models import Product, User
-from base.products import products
-from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.models import Product, Order, OrderItem, ShippingAddress 
+
+from base.serializers import ProductSerializer, OrderSerializer
+
+@api_view(['POST'])
+@permission_classes(['IsAuthenticated'])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+    
+    orderItems = data['orderItems']
+    if orderItems and len (orderItems) == 0:
+        return Response({'detail':'No Order Items'}, status = status.HTTP_400_BAD_REQUEST)
+    
+    else:
+        #Create order
+        order = Order.objects.create(
+            user = user,
+            paymentMethod = data['paymentMethod'],
+            taxPrice = data['taxPrice'],
+            shippingPrice = data['shippingPrice'],
+            totalPrice = data['totalPrice']
+        )
+        
+        #create shipping address
+        shipping = ShippingAddress.objects.create (
+            order = order,
+            address = data['shippingAddress']['address'],
+            city = data['shippingAddress']['city'],
+            postalCode= data['shippingAddress']['zipCode'],
+            country= data['shippingAddress']['country']
+        )
+        #create order items by iterating through list 
+        for i in orderItems:
+            product = Product.objects.get(id = i['product'])
+            
+            item = OrderItem.objects.create(
+                product= product,
+                order= order,
+                name= product.name,
+                qty= i['qty'],
+                price=i['price'],
+                image= product.image.url, 
+            )
+         #update stock (still inside the loop)
+            product.countInStock -= item.qty
+            product.save()
+        
+    serializer = OrderSerializer(order, many = True)
+    return Response(serializer.data)
